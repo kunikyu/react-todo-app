4b17812c54b1982ad5eed76cf4975beb53bfdfcf
kunikyu <106215442+kunikyu@users.noreply.github.com>
2024-11-27 14:34:23 +0900

    きれいにした

--- src/LiePop.tsx
+++ src/LiePop.tsx
@@ -18,39 +18,24 @@ const LiePop = (props: Props) => {
   return (
     <div>
       {isPopUpVisible !== 0 && (
-        <div className="fixed left-0 top-0 z-40 size-full bg-black/50"></div>
-      )}
-      {isPopUpVisible === 1 && (
-        <div className="fixed left-1/2 top-1/2 z-50 size-full max-w-2xl -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray-100 p-5 shadow-lg sm:h-2/3">
-          <p>嘘課題はおわらせることができないよねー。嘘なんだから</p>
-          <button
-            onClick={togglePopUp}
-            className="ml-auto rounded-md bg-black px-2 py-1 text-sm font-bold text-white "
-          >
-            閉じる
-          </button>
-        </div>
-      )}
-      {isPopUpVisible === 2 && (
-        <div className="fixed left-1/2 top-1/2 z-50 size-full max-w-2xl -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray-100 p-5 shadow-lg sm:h-2/3">
-          <p>嘘は消すことができないんだよー。いつか消えるといいね</p>
-          <button
-            onClick={togglePopUp}
-            className="ml-auto rounded-md bg-black px-2 py-1 text-sm font-bold text-white "
-          >
-            閉じる
-          </button>
-        </div>
-      )}
-      {isPopUpVisible === 3 && (
-        <div className="fixed left-1/2 top-1/2 z-50 size-full max-w-2xl -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray-100 p-5 shadow-lg sm:h-2/3">
-          <p>嘘に嘘を重ねるつもりなの？それに何の意味があるんだい</p>
-          <button
-            onClick={togglePopUp}
-            className="ml-auto rounded-md bg-black px-2 py-1 text-sm font-bold text-white "
-          >
-            閉じる
-          </button>
+        <div className="fixed inset-0 z-40 flex items-center justify-center bg-black/50">
+          <div className="size-full max-w-2xl rounded-md bg-gray-100 p-5 shadow-lg sm:h-2/3">
+            {isPopUpVisible === 1 && (
+              <p>嘘課題はおわらせることができないよねー。嘘なんだから</p>
+            )}
+            {isPopUpVisible === 2 && (
+              <p>嘘は消すことができないんだよー。いつか消えるといいね</p>
+            )}
+            {isPopUpVisible === 3 && (
+              <p>嘘に嘘を重ねるつもりなの？それに何の意味があるんだい</p>
+            )}
+            <button
+              onClick={togglePopUp}
+              className="mt-3 rounded-md bg-red-500 p-2 text-white"
+            >
+              閉じる
+            </button>
+          </div>
         </div>
       )}
     </div>
